efd4369236ebeb49d48036df3684016c5198b4f0
Lucas Bittencurt <lucasfilipeb@gmail.com>
2023-08-10 11:27:46 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     // limit of 25 deviceSizes values
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
