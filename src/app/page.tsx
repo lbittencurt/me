dc9259ba96f20a6adbb5a7100fb24d8bd0e45966
Lucas Bittencurt <lucasfilipeb@gmail.com>
2023-08-10 11:04:32 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,255 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import { IconContext } from 'react-icons'
+import { HiOutlineEnvelope } from 'react-icons/hi2'
+import { SiAmazonaws, SiReact, SiNodedotjs, SiTypescript, SiRuby, SiDocker } from 'react-icons/si'
+{/* <IconContext.Provider value={{ color: "blue", className: "global-class-name" }}>
+  <div>
+    <FaFolder />
+  </div>
+</IconContext.Provider>; */}
+
+{/* <Image
+  src="/vercel.svg"
+  alt="Vercel Logo"
+  className="dark:invert"
+  width={100}
+  height={24}
+  priority
+/> */}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div
+      className='
+        max-w-4xl
+        flex
+        flex-col
+        justify-between
+        m-auto
+        p-4
+        h-full
+      '
+    >
+      <div
+        className='
+          flex
+          flex-col
+        '
+      >
+        <header
+          className='
+            w-full
+            flex
+            justify-between
+            pt-2
+            pb-8
+          '
+        >
+          {/* <IconContext.Provider value={{ color: "blue", className: "global-class-name" }}> */}
+          {/* </IconContext.Provider> */}
+          <div
+            className='flex items-center gap-x-2'
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            <HiOutlineEnvelope />
+            <span
+              className='
+                text-md
+                text-slate-200
+              '
+            >lucasfilipeb@gmail.com</span>
+          </div>
+          <Link
+            href="https://www.linkedin.com/in/lucasbittencurt/"
+            target='_blank'
+            className='
+              hover:text-blue-400
+              transition
+              text-md
+              text-slate-200
+            '
+          >LinkedIn</Link>
+        </header>
+        <main
+          className='
+            flex
+            flex-col
+            items-center
+            justify-center
+            py-6
+          '
+        >
+          <Image
+            src="/me.jpeg"
+            alt="Lucas Bittencurt photo"
+            className="
+              rounded-full
+              border-none
+              align-middle
+            "
+            width={140}
+            height={24}
+            priority
+          />
+          <span
+            className='text-sm text-slate-400 pt-2'
+          >{`Hey, I'm Lucas `}👋</span>
+          <p
+            className='text-2xl max-w-sm text-center pt-2 text-slate-200'
+          >Crafting seamless experiences with code quality</p>
+        </main>
+        <section
+          className='
+            flex
+            items-center
+            justify-center
+            py-8
+          '
+        >
+          <span
+            className='
+              text-md
+              text-slate-200
+            '
+          >Stack</span>
+          <div className='w-[1px] h-[20px] bg-slate-600 mx-4' />
+          <div
+            className='
+              flex
+              gap-x-5
+            '
+          >
+            <SiReact size={22} alt="React icon" ariaLabel="React" />
+            <SiNodedotjs size={22} alt="Node.js icon" ariaLabel="Node.js" />
+            <SiTypescript size={22} alt="Typescript icon" ariaLabel="Typescript" />
+            <SiRuby size={22} alt="Ruby icon" ariaLabel="Ruby" />
+            <SiAmazonaws size={22} alt="AWS icon" ariaLabel="AWS" />
+            <SiDocker size={22} alt="Docker icon" ariaLabel="Docker" />
+          </div>
+        </section>
+        <section
+          className='
+            flex
+            justify-center
+            md:justify-between
+            w-full
+            pt-6
+            flex-wrap
+            gap-4
+          '
+        >
+          <div
+            className='
+              flex
+              flex-col
+            '
+          >
+            <span
+              className='
+                text-lg
+                text-slate-200
+                font-medium
+                text-center
+                md:text-left
+              '
+            >Professional</span>
+            <div
+              className='
+                border
+              border-slate-600
+              bg-slate-900/80
+                text-sm
+                text-slate-200
+                p-5
+                rounded-md
+                mt-2
+                max-w-[400px]
+              '
+            >
+              {`
+                Experienced software engineer specialized in project and application development for a decade. Proficient in Cloud (AWS, Azure), Startup culture, DevOps, scalability, and small team management. Worked with monolithic apps and microservices in diverse settings. A constant learner, intrigued by technology choices and committed to sharing insights.
+              `}
+            </div>
+          </div>
+          <div
+            className='
+              flex
+              flex-col
+            '
+          >
+            <span
+              className='
+                text-lg
+                text-slate-200
+                font-medium
+                text-center
+                md:text-left
+              '
+            >Contracts with</span>
+            <div className='flex gap-3'>
+              <div
+                className='
+                  flex
+                  items-center
+                  justify-center
+                  border
+                border-slate-600
+                bg-slate-900/80
+                  p-5
+                  rounded-md
+                  mt-2
+                  min-w-[168px]
+                '
+              >
+                <Image
+                  src="/nearform_logo.png"
+                  alt="NearForm logo"
+                  width={130}
+                  height={25}
+                  priority
+                />
+              </div>
+              <div
+                className='
+                  flex
+                  items-center
+                  justify-center
+                  border
+                border-slate-600
+                bg-slate-900/80
+                  p-5
+                  rounded-md
+                  mt-2
+                  min-w-[168px]
+                  opacity-50
+                '
+              >
+                <Image
+                  src="/upwork_logo.png"
+                  alt="Upwork logo"
+                  width={90}
+                  height={20}
+                  priority
+                />
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <footer
+        className='
+          flex
+          flex-col
+          items-center
+          justify-center
+          w-full
+          text-slate-300
+          py-4
+        '
+      >
+        <span>...</span>
+        <span className='text-xs'>All rights reserved to Bittencurt Software Consultancy LTDA</span>
+      </footer>
+    </div>
   )
 }
